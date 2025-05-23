3f37d9b252632e745508d6d162f0816468c6af65
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-05-24 00:01:44 +0300

    Добавлен комментарий

--- src/ru/aston/hw2/MyLInkedList.java
+++
@@ -1,8 +0,0 @@
-package ru.aston.hw2;
-
-import java.util.LinkedList;
-
-public class MyLInkedList {
-
-    LinkedList list = new LinkedList<>();
-}
---
+++ src/ru/aston/hw2/MyLinkedList.java
@@ -0,0 +1,6 @@
+package ru.aston.hw2;
+
+
+public class MyLinkedList {
+    // UPD 24.05: Если успею, то сделаю реализацию связного списка
+}
