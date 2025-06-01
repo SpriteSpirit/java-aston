0973f08292acb6402bc5a4fac00ac541ec424f92
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-06-02 00:27:58 +0300

    Реализован класс для кастомной обработки исключений

---
+++ src/ru/aston/hw3/FileOperationException.java
@@ -0,0 +1,12 @@
+package ru.aston.hw3;
+
+
+/**
+ * Настраиваемое исключения при работе с файлами.
+ */
+public class FileOperationException extends Exception {
+
+    public FileOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
