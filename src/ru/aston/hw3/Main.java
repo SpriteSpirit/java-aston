9b7e04debebaa126bd7fdaaf2eacff4d9dc3afb8
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-06-02 00:35:30 +0300

    Выполнена запись в файл

---
+++ src/ru/aston/hw3/Main.java
@@ -0,0 +1,38 @@
+package ru.aston.hw3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tT] [%4$-7s] %5$s %n");
+
+        String filePath = "src/ru/aston/hw3/exampleFile.txt";
+        FileOperations fileOperations = new FileOperations(filePath);
+
+        List<String> dataList = new ArrayList<>();
+
+        dataList.add("Первая строка");
+        dataList.add("Вторая строка");
+        dataList.add("Третья строка");
+
+        processWriting(fileOperations, dataList);
+    }
+
+    public static void processWriting(FileOperations operations, List<String> list) {
+        try {
+            operations.writeToFile(list);
+        } catch (FileOperationException e) {
+            logger.severe("Ошибка при записи: " + e.getMessage());
+
+            if (e.getCause() != null) {
+                logger.severe("Причина ошибки: " + e.getCause().getMessage());
+            }
+        }
+    }
+}
