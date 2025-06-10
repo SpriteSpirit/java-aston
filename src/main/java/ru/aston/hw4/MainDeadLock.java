1cc0c6360c584f3a263f7d5b022b592af812d7b4
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-06-10 13:16:06 +0300

    Выполнен рефакторинг: класс Main разбит на 2 класса

--- src/main/java/ru/aston/hw4/MainDeadLock.java
+++ src/main/java/ru/aston/hw4/MainDeadLock.java
@@ -3,7 +3,7 @@ package main.java.ru.aston.hw4;
 
 import main.java.ru.aston.hw4.DeadLock.Friend;
 
-public class Main {
+public class MainDeadLock {
 
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tT] [%4$-7s] %5$s %n");
