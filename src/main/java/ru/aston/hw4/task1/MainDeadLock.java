148b00326a8d7e21f83d8e83f37f82a33362afce
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-06-10 18:26:27 +0300

    Исправлена структура

--- src/main/java/ru/aston/hw4/task1/MainDeadLock.java
+++ src/main/java/ru/aston/hw4/task1/MainDeadLock.java
@@ -1,7 +1,7 @@
-package main.java.ru.aston.hw4;
+package main.java.ru.aston.hw4.task1;
 
 
-import main.java.ru.aston.hw4.DeadLock.Friend;
+import main.java.ru.aston.hw4.task1.DeadLock.Friend;
 
 public class MainDeadLock {
 
