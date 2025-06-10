5b3e63350bfab48bb364bfffb46c85402fc8c5c1
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-06-10 18:30:36 +0300

    Реализован метод для запуска первого потока

--- src/main/java/ru/aston/hw4/task2/TwoThreads.java
+++ src/main/java/ru/aston/hw4/task2/TwoThreads.java
@@ -1,5 +1,8 @@
 package main.java.ru.aston.hw4.task2;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +13,9 @@ import java.util.logging.Logger;
 public class TwoThreads {
 
     private static final Logger logger = Logger.getLogger(TwoThreads.class.getName());
+
+    private static final Lock lock = new ReentrantLock();
+    private static final Condition condition = lock.newCondition();
 
     private volatile boolean isFirstTurn = true;
     private volatile boolean isRunning = true;
@@ -24,6 +30,38 @@ public class TwoThreads {
             logger.info(String.format("%s: %s", threadName, String.format(message, args)));
         }
     }
+
+    /**
+     * Выполнение первого потока, который выводит "1".
+     */
+    public void captureThread1() {
+        String threadName = "Thread1";
+
+        while (isRunning) {
+            lock.lock();
+
+            try {
+                if (!isFirstTurn) {
+                    condition.await();
+                }
+
+                if (!isRunning) {
+                    break;
+                }
+
+                loggerInfo(threadName, "1");
+                isFirstTurn = false;
+                // передача сигнала второму потоку.
+                condition.signal();
+            } catch (InterruptedException e) {
+                logger.log(Level.SEVERE, "Поток 1 прерван. Ошибка: ", e);
+                Thread.currentThread().interrupt();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
 
     /**
      * Возвращает строковое представление объекта.
